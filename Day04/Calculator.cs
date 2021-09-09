0d85fe0e95d4a8e2ff33a7ae22d8f9b24bd35a0d
Shreyas2797 <90186549+Shreyas2797@users.noreply.github.com>
2021-09-09 18:41:38 +0530

    Add files via upload

---
+++ Day04/Calculator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Assignment1.Day02;
+
+namespace Assignment1.Day04
+{
+    class Calculator
+    {
+
+        // 1. Calculator program using simple switch case in a main function only
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Enter the first number");
+        //    int number1 = int.Parse(Console.ReadLine());
+        //    Console.WriteLine("Enter the second number");
+        //    int number2 = int.Parse(Console.ReadLine());
+        //    Console.WriteLine("Calculator");
+        //    Console.WriteLine(" 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division");
+        //    switch (int.Parse(Console.ReadLine()))
+        //    {
+        //        case 1:
+        //            Console.WriteLine(number1 + number2);
+        //            break;
+
+        //        case 2: Console.WriteLine(number1 - number2);
+        //            break;
+        //        case 3:
+        //            Console.WriteLine(number1 * number2);
+        //            break;
+
+        //        case 4:
+        //            if(number2 != 0)
+        //                Console.WriteLine(number1 / number2);
+        //            else
+        //                Console.WriteLine("Can't Divide by 0");
+        //            break;
+        //        default:
+        //            break;
+        //    }
+        //}
+
+        static void Main(string[] args)
+        {
+            int number1 = MyConsole.GetNumber("Enter the first number");            
+            int number2 = MyConsole.GetNumber("Enter the second number");
+            Console.WriteLine("Calculator");
+            Console.WriteLine(" 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division ");
+            switch (int.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    Addition(number1,number2);                   
+                    break;
+                case 2:
+                    Subtraction(number1, number2);
+                    break;
+                case 3:
+                    Multiplication(number1, number2);
+                    break;
+                case 4:
+                    Division(number1, number2);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void Division(int num1, int num2)
+        {
+            if (num2 != 0)
+                Console.WriteLine(num1 / num2);
+            else
+                Console.WriteLine("Can't Divide by 0");
+        }
+
+        private static void Multiplication(int num1, int num2)
+        {
+            Console.WriteLine(num1 * num2 ); 
+        }
+
+        private static void Subtraction(int num1, int num2)
+        {
+            Console.WriteLine(num1 - num2);
+        }
+
+        private static void Addition( int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2); 
+        }
+    }
+}
---
+++ Day04/PracticalSession.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace Assignment1.Day04
+{
+    class PracticalSession
+    {
+        static void Main(string[] args)
+        {
+            //-------first program------
+
+            //Console.WriteLine("My name is Shreyas");
+            //Console.WriteLine("I am from Bangalore");
+            //Console.WriteLine("I am a trainee working in Fidelity");
+
+
+            //--------Second program(getting data from user)--
+
+            //Console.WriteLine("Enter your name");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Enter your address");
+            //string address = Console.ReadLine();
+            //Console.WriteLine("Enter your Qualification");
+            //string qualification = Console.ReadLine();
+            //Console.WriteLine($"The user name is {name} from {address} with a qualification of {qualification}");
+
+
+            //----Third Program(DataTypes and conversion)---
+
+            //Console.WriteLine("Enter the Principal amount");
+            //int principle = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the term");
+            //double term = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the rate of interest");
+            //double rate = double.Parse(Console.ReadLine());
+
+            //double interest = principle * term * rate;
+            //int SimpleInterest = Convert.ToInt32(interest);
+            //Console.WriteLine($"interest is {interest}");
+            //Console.WriteLine($"SimpleInterest is {SimpleInterest}");
+
+
+            //----Fourth Program(DateTime conversion)------
+            //Console.WriteLine("Enter your name");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Enter your DateOFBirth in mm/dd/yyyy");
+            //DateTime dob =DateTime.Parse( Console.ReadLine());
+            //int age = DateTime.Now.Year - dob.Year;
+            //Console.WriteLine($"The name of the user is {name} and his age is {age}");
+
+
+            //-----Fifth Program(Using TryParse)---------
+            int age;
+            RETRY:
+            Console.WriteLine("Enter the Age");
+            if (!int.TryParse(Console.ReadLine(), out age))
+            {
+                Console.WriteLine($"U R supposed to enter a whole number ranging from {int.MinValue} and {int.MaxValue}");
+                goto RETRY;
+            }
+            Console.WriteLine($"The age is {age}");
+
+        }
+    }
+}
